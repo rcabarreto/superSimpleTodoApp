5f7dcb26cc5f4519c2211bc17ee6fb833183e093
Rodrigo Barreto <rcabarreto@gmail.com>
2018-07-03 17:54:52 -0300

    testing

---
+++ app/tests/components/Todo.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Todo from '../../components/Todo'
+
+describe('Test the Todo component', () => {
+
+  it('Component should exist', () => {
+    expect(Todo).toBeTruthy();
+  });
+
+});
